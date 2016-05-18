98667a2327bb225eab2d10d3f595ba8c29620d67
Benjamin LRX <benjamin.l@hotmail.fr>
2016-05-18 14:52:51 +0200

    Add specific test_function callback

--- usb/user/usbd_cdc_if.c
+++ usb/user/usbd_cdc_if.c
@@ -152,68 +152,68 @@ static int8_t CDC_DeInit_FS(void)
   */
 static int8_t CDC_Control_FS  (uint8_t cmd, uint8_t* pbuf, uint16_t length)
 { 
-  /* USER CODE BEGIN 5 */
-  switch (cmd)
-  {
-  case CDC_SEND_ENCAPSULATED_COMMAND:
+    /* USER CODE BEGIN 5 */
+    switch (cmd)
+    {
+    case CDC_SEND_ENCAPSULATED_COMMAND:
+      
+	break;
+      
+    case CDC_GET_ENCAPSULATED_RESPONSE:
+      
+	break;
+
+    case CDC_SET_COMM_FEATURE:
  
-    break;
+	break;
 
-  case CDC_GET_ENCAPSULATED_RESPONSE:
- 
-    break;
+    case CDC_GET_COMM_FEATURE:
 
-  case CDC_SET_COMM_FEATURE:
- 
-    break;
+	break;
 
-  case CDC_GET_COMM_FEATURE:
+    case CDC_CLEAR_COMM_FEATURE:
 
-    break;
+	break;
 
-  case CDC_CLEAR_COMM_FEATURE:
-
-    break;
-
-  /*******************************************************************************/
-  /* Line Coding Structure                                                       */
-  /*-----------------------------------------------------------------------------*/
-  /* Offset | Field       | Size | Value  | Description                          */
-  /* 0      | dwDTERate   |   4  | Number |Data terminal rate, in bits per second*/
-  /* 4      | bCharFormat |   1  | Number | Stop bits                            */
-  /*                                        0 - 1 Stop bit                       */
-  /*                                        1 - 1.5 Stop bits                    */
-  /*                                        2 - 2 Stop bits                      */
-  /* 5      | bParityType |  1   | Number | Parity                               */
-  /*                                        0 - None                             */
-  /*                                        1 - Odd                              */ 
-  /*                                        2 - Even                             */
-  /*                                        3 - Mark                             */
-  /*                                        4 - Space                            */
-  /* 6      | bDataBits  |   1   | Number Data bits (5, 6, 7, 8 or 16).          */
-  /*******************************************************************************/
-  case CDC_SET_LINE_CODING:   
+	/*******************************************************************************/
+	/* Line Coding Structure                                                       */
+	/*-----------------------------------------------------------------------------*/
+	/* Offset | Field       | Size | Value  | Description                          */
+	/* 0      | dwDTERate   |   4  | Number |Data terminal rate, in bits per second*/
+	/* 4      | bCharFormat |   1  | Number | Stop bits                            */
+	/*                                        0 - 1 Stop bit                       */
+	/*                                        1 - 1.5 Stop bits                    */
+	/*                                        2 - 2 Stop bits                      */
+	/* 5      | bParityType |  1   | Number | Parity                               */
+	/*                                        0 - None                             */
+	/*                                        1 - Odd                              */ 
+	/*                                        2 - Even                             */
+	/*                                        3 - Mark                             */
+	/*                                        4 - Space                            */
+	/* 6      | bDataBits  |   1   | Number Data bits (5, 6, 7, 8 or 16).          */
+	/*******************************************************************************/
+    case CDC_SET_LINE_CODING:   
 	
-    break;
+	break;
 
-  case CDC_GET_LINE_CODING:     
+    case CDC_GET_LINE_CODING:     
 
-    break;
+	break;
 
-  case CDC_SET_CONTROL_LINE_STATE:
+    case CDC_SET_CONTROL_LINE_STATE:
 
-    break;
+	break;
 
-  case CDC_SEND_BREAK:
+    case CDC_SEND_BREAK:
  
-    break;    
+	break;    
     
-  default:
-    break;
-  }
+    default:
+	break;
+    }
 
-  return (USBD_OK);
-  /* USER CODE END 5 */
+    return (USBD_OK);
+    /* USER CODE END 5 */
 }
 
 /**
@@ -258,10 +258,14 @@ static int8_t CDC_Receive_FS (uint8_t* Buf, uint32_t *Len)
 	USBD_CDC_SetRxBuffer(hUsbDevice_0, &buff_RX[0]);
   	USBD_CDC_ReceivePacket(hUsbDevice_0);
 
-	led_test1();
-	led_test2();
-	led_test3();
-	
+	if (buff_TX[0] == '1') {
+	    led_test1();
+	} else if (buff_TX[0] == '2') {
+	    led_test2();
+	} else if (buff_TX[0] == '3') {
+	    led_test3();
+	}
+
 	return (result);
   /* USER CODE END 6 */ 
 }
