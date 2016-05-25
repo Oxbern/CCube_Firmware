9676ee13540e29295aa31ae73b847db1cfb5e7cf
Benjamin LRX <benjamin.l@hotmail.fr>
2016-05-25 22:01:29 +0200

    Set USB IRQ level to 2 (to be tested)

--- usb/user/usbd_conf.c
+++ usb/user/usbd_conf.c
@@ -86,7 +86,7 @@ void HAL_PCD_MspInit(PCD_HandleTypeDef* hpcd)
     __USB_OTG_FS_CLK_ENABLE();
 
     /* Peripheral interrupt init*/
-    HAL_NVIC_SetPriority(OTG_FS_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(OTG_FS_IRQn, 2, 2);
     HAL_NVIC_EnableIRQ(OTG_FS_IRQn);
   /* USER CODE BEGIN USB_OTG_FS_MspInit 1 */
 
